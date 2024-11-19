0dcbfc94d1769ee312c29dbc380a781b7f09665f
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-11-19 13:19:30 -0500

    Feat(init): init mongodb and setup to use models, migrations, and seeders for dev and prod

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.local"
+});
+
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const port = process.env.PORT;
 
 const isProduction = process.env.NODE_ENV === "production";
 
 // Standard middleware
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// Logging middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// Serve static files in production
 // Serve static files in production
 if (isProduction) {
   const staticPath = path.resolve(__dirname, "../web");
@@ -21,16 +23,22 @@ if (isProduction) {
   app.use(express.static(staticPath));
 }
 
-const indexPath = path.resolve(__dirname, "../web", "index.html");
-console.log("Index Path:", indexPath);
-
 // Catch-all route for frontend
 app.get(/^((?!\/api).)*$/, (req, res) => {
   console.log("Serving index.html");
   res.sendFile(indexPath);
 });
 
-const port = 4000;
-app.listen(port, () => {
-  console.log(`Listening for requests on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    console.log("MONGO_URI", process.env.MONGODB_URI);
+
+    app.listen(port, () => {
+      console.log(`Listening for requests on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
